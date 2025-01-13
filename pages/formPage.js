a16ad75885cb0bea93eeb5f110a71077c79d1ec4
e.kochetova <pandoraizida@gmail.com>
2025-01-13 14:26:01 +0100

    fixes after second review, lints

--- pages/formPage.js
+++ pages/formPage.js
@@ -4,51 +4,53 @@ import { expect } from 'vitest';
 export default class FormElements {
   constructor(screen) {
     this.screen = screen;
-    this.emailField = this.screen.getByLabelText('Email')
-    this.passwordField = this.screen.getByLabelText('Пароль')
-    this.adressField = this.screen.getByLabelText('Адрес')
-    this.cityField = this.screen.getByLabelText('Город')
-    this.countryDropdown = this.screen.getByLabelText('Страна')
-    this.countrySelectedValue = this.screen.getByDisplayValue('Выберите')
-    this.acceptCheckbox = this.screen.getByRole('checkbox', { name: 'Принять правила' })
+    this.emailField = this.screen.getByLabelText('Email');
+    this.passwordField = this.screen.getByLabelText('Пароль');
+    this.adressField = this.screen.getByLabelText('Адрес');
+    this.cityField = this.screen.getByLabelText('Город');
+    this.countryDropdown = this.screen.getByLabelText('Страна');
+    this.countrySelectedValue = this.screen.getByDisplayValue('Выберите');
+    this.acceptCheckbox = this.screen.getByRole('checkbox', { name: 'Принять правила' });
   }
 
   async currentButton(name) {
-    return this.screen.getByRole('button', { name: name })
+    return this.screen.getByRole('button', { name });
   }
 
   async acceptedRules(state) {
-    return this.screen.getByText(state)
+    return this.screen.getByText(state);
   }
 
   async findAllElements(role) {
-    return this.screen.findAllByRole(role)
+    return this.screen.findAllByRole(role);
   }
 
   async currentTable() {
-    return this.screen.getByRole('table')
+    return this.screen.getByRole('table');
   }
 
   async typeValue(input, text) {
-    fireEvent.change(input, {target: {value: text} })
+    fireEvent.change(input, { target: { value: text } });
   }
 
   async clickButton(button) {
-    fireEvent.click(await this.currentButton(button))
+    fireEvent.click(await this.currentButton(button));
   }
 
   async clickCheckbox() {
-    fireEvent.click(await this.acceptCheckbox)
+    fireEvent.click(await this.acceptCheckbox);
   }
 
   async expectTableTitlesVisibility(expectedArray, condition = true) {
     expectedArray.forEach(async (elem) => {
-      condition === true ? expect(await this.currentTable()).toHaveTextContent(elem) : expect(await this.currentTable()).not.toHaveTextContent(elem)    
-    })
+      condition === true 
+        ? expect(await this.currentTable()).toHaveTextContent(elem)
+        : expect(await this.currentTable()).not.toHaveTextContent(elem);
+    });
   }
 
   async expectUserDataVisability(obj, condition) {
-    const expectedArray = Object.values(obj)
-    this.expectTableTitlesVisibility(expectedArray, condition)
+    const expectedArray = Object.values(obj);
+    this.expectTableTitlesVisibility(expectedArray, condition);
   }
 }
--- pages/widgetPage.js
+++ pages/widgetPage.js
@@ -1,34 +1,34 @@
-import { fireEvent } from '@testing-library/react'
-import { expect } from 'vitest'
+import { fireEvent } from '@testing-library/react';
+import { expect } from 'vitest';
 
 export default class WidgetElements {
   constructor(screen) {
     this.screen = screen;
-    this.wrappedWidgetButton = this.screen.getByRole('button', { name: 'Открыть Чат' })
+    this.wrappedWidgetButton = this.screen.getByRole('button', { name: 'Открыть Чат' });
   }
 
   async currentWidgetButton(name) {
-    return this.screen.getByRole('button', { name: name })
+    return this.screen.getByRole('button', { name });
   }
 
   async widgetAvatar() {
-    return this.screen.getByAltText('tota')
+    return this.screen.getByAltText('tota');
   }
 
   async modalHeaderText() {
-    return this.screen.getByText('Виртуальный помощник')
+    return this.screen.getByText('Виртуальный помощник');
   }
 
   async closeWidgetButton() {
-    return this.screen.getByRole('button', { name: 'Close' })
+    return this.screen.getByRole('button', { name: 'Close' });
   }
 
   async getTagForText(text) {
-    return this.screen.getByText(text).tagName
+    return this.screen.getByText(text).tagName;
   }
 
   async clickWidgetButton(button) {
-    fireEvent.click(await this.currentWidgetButton(button))
+    fireEvent.click(await this.currentWidgetButton(button));
   }
 
   async expectCurrenButtonVisability(text) {
@@ -36,8 +36,8 @@ export default class WidgetElements {
   }
 
   expectLastMessageVisability(text) {
-    const messages = this.screen.getAllByText(text)
-    const lastMessage = messages[messages.length - 1]
-    expect(lastMessage).toBeVisible()
+    const messages = this.screen.getAllByText(text);
+    const lastMessage = messages[messages.length - 1];
+    expect(lastMessage).toBeVisible();
   }
 }
